ae8123de44abdfbdba1eff5c8b6ef89e047a5be6
zcrosman <21688962+zcrosman@users.noreply.github.com>
2021-02-27 12:21:04 -0600

    Update autoHC.bat

--- autoHC.bat
+++ autoHC.bat
@@ -1,15 +1,16 @@
-set wordlistdir=D:\hashcat-6.0.0\hashcat-6.0.0\wordlist.txt
+set wordlistdir=D:\hashcat-6.0.0\hashcat-6.0.0\Top1pt8Billion-WPA-probable-v2.txt
 set hcdir=D:\hashcat-6.0.0\hashcat-6.0.0
 set handdir=D:\hashcat-6.0.0\hashcat-6.0.0\handshakes
 
 
 if not exist %handdir%\cap mkdir %handdir%\cap
 
-for /f %%f in ('dir /b /s %handdir%\*')^ 
- do %hcdir%\hashcat-utils-1.9\bin\cap2hccapx.exe %%f %handdir%\cap\%%~nf.hccapx
+for /f %%f in ('dir /b /s %handdir%\*') ^
+do %hcdir%\hashcat-utils-1.9\bin\cap2hccapx.exe %%f %handdir%\cap\%%~nf.hccapx
 
 PAUSE
 
-for /f %%f in ('dir /b /s D:\EH\hashcat-6.0.0\hashcat-6.0.0\handshakes\cap\*') do %hcdir%\hashcat.exe -m 2500 %%f %wordlist%
+for /f %%f in ('dir /b /s D:\EH\hashcat-6.0.0\hashcat-6.0.0\handshakes\cap\*') ^
+do %hcdir%\hashcat.exe -m 2500 %%f %wordlist%
 
 PAUSE
